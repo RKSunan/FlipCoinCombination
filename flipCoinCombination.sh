87aa8094c90508776a79d6f69a96b5505f62fc2c
RKSunan <rajkumarisunanda@gmail.com>
2020-11-03 22:36:29 +0530

    DoubleFlippingofCoinMultipleTimes

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,22 +1,41 @@
 #! /bin/bash
-head=0
-tail=0
+HH=0
+HT=0
+TH=0
+TT=0
 declare -A Dict
 for i in $(seq 1 50)
 do
-flip=$((RANDOM%2))
-if [ $flip == 1 ]
+flip1=$((RANDOM%2))
+flip2=$((RANDOM%2))
+if [[ $flip1 == 1 && $flip2 == 1 ]]
 then
-	Dict[$i]="H"
-	((head++))
+	Dict[$i]="HH"
+	((HH++))
+elif [[ $flip1 == 1 && $flip2 == 0 ]]
+then
+        Dict[$i]="HT"
+        ((HT++))
+elif [[ $flip1 == 0 && $flip2 == 1 ]]
+then
+        Dict[$i]="TH"
+        ((TH++))
+
 else
-	Dict[$i]="T"
-	((tail++))
+	Dict[$i]="TT"
+	((TT++))
 fi
 done
-echo "H : $head"
-echo "T : $tail"
-h=`expr $head \* 100 / 50 `
-echo "Percentage of Head is $h % "
-t=`expr $tail \* 100 / 50`
-echo "Percentahe of Tail is $t %" 
+echo "HH : $HH"
+echo "HT : $HT"
+echo "TH : $TH"
+echo "TT : $TT"
+HH=`expr $HH \* 100 / 50 `
+echo "Percentage of HH is $HH % "
+HT=`expr $HT \* 100 / 50`
+echo "Percentahe of HT is $HT %" 
+TH=`expr $TH \* 100 / 50 `
+echo "Percentage of TH is $TH % "
+TT=`expr $TT \* 100 / 50`
+echo "Percentahe of TT is $TT %"
+
