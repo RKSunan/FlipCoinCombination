daf6ac360e5428502b95bdf69472ee51602ae864
RKSunan <rajkumarisunanda@gmail.com>
2020-11-03 22:17:37 +0530

    SignletFlippingaCoinMultipleTimes

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,8 +1,22 @@
 #! /bin/bash
+head=0
+tail=0
+declare -A Dict
+for i in $(seq 1 50)
+do
 flip=$((RANDOM%2))
 if [ $flip == 1 ]
 then
-	echo "Head "
+	Dict[$i]="H"
+	((head++))
 else
-	echo "Tail"
+	Dict[$i]="T"
+	((tail++))
 fi
+done
+echo "H : $head"
+echo "T : $tail"
+h=`expr $head \* 100 / 50 `
+echo "Percentage of Head is $h % "
+t=`expr $tail \* 100 / 50`
+echo "Percentahe of Tail is $t %" 
